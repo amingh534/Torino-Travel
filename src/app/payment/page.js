89ff0fca77e064beef996f82dd8c487f48e347ff
Amin Ghashghai <amingh534@gmail.com>
2025-12-14 01:27:52 +0330

    Payment page developed

--- src/app/payment/page.js
+++ src/app/payment/page.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
+import React, { useEffect } from "react";
 
 function PaymentPage() {
+  const { getQuery } = useQuery();
+  const status = getQuery("status");
+  if (status === "success")
+    return (
+      <div>
+        <p>پرداخت شما با موفقیت انجام شد</p>
+        <Link href="/profile/my-tours">برو به پروفایل</Link>
+      </div>
+    );
   return (
-    <div>PaymentPage</div>
-  )
+    <div>
+      <p>پرداخت انجام نشد</p>
+    </div>
+  );
+  console.log(status); 
+  useEffect(() => {}, []);
+  return <div>PaymentPage</div>;
 }
 
-export default PaymentPage
+export default PaymentPage;

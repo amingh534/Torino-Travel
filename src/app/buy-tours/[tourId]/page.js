564d5826a9d5dac1959ceff0766326c947f5b782
Amin Ghashghai <amingh534@gmail.com>
2025-12-26 03:38:12 +0330

    Profile Responsive updated

--- src/app/buy-tours/[tourId]/page.js
+++ src/app/buy-tours/[tourId]/page.js
@@ -1,11 +1,17 @@
 import ToursDetailPage from "@/template/ToursDetailPage";
 import React from "react";
 import { serverFetch } from "src/components/core/services/http";
+import AuthProvider from "src/components/partials/provider/AutProvider";
 
 async function ToursDetails({ params }) {
   const { tourId } = params;
   const data = await serverFetch(`tour/${tourId}`, null, { cache: "no-store" });
-  return <ToursDetailPage detailsData={data} />;
+  console.log(data);
+  return (
+    <AuthProvider>
+      <ToursDetailPage detailsData={data} />;
+    </AuthProvider>
+  );
 }
 
 export default ToursDetails;
--- src/components/template/ProfilePage.js
+++ src/components/template/ProfilePage.js
@@ -7,14 +7,14 @@ function ProfilePage({ children }) {
   return (
     <div
       className="flex justify-center  gap-x-6 mt-10 mb-20 
+      max-lg:flex-col
       max-md:flex
   max-md:flex-col 
   max-w-[1440px] 
-  mx-auto"
+  mx-auto "
     >
       <ul
         className="flex flex-col mt-10 justify-evenly
-   bg-[#F5F5F5]
     font-['Yekan_Bakh'] text-lg
     divide-y
     divide-black/10
@@ -22,21 +22,31 @@ function ProfilePage({ children }) {
     rounded-[10px]
     border border-[#00000033]
     w-[284px]
+    max-lg:flex-row
+    max-lg:w-full
+    max-lg:border-none
+    max-lg:divide-y-0
+    max-lg:h-[70px]
     max-h-[170px]
     max-md:flex-row
   max-md:w-full
   max-md:justify-around
   max-md:rounded-none
   max-md:divide-none
+  max-md:border-none
  
-  
   "
       >
         <li
-          className="inline-flex  max-md:hover:border-b-2 max-md:border-b-[#28A745]  p-2 
-  hover:bg-[#28A74540]
-  hover:text-[#28A745]
-  md:hover:rounded-t-[9px]"
+          className="
+inline-flex items-center p-2
+md:hover:bg-[#28A74540]
+md:hover:text-[#28A745]
+md:hover:rounded-t-[9px]
+
+
+max-md:hover:text-[#28A745]
+"
         >
           <Profile />
           <Link className="pr-3 " href="/profile">
@@ -44,13 +54,32 @@ function ProfilePage({ children }) {
           </Link>
         </li>
 
-        <li className="inline-flex max-md:hover:underline  p-2 hover:bg-[#28A74540]  hover:text-[#28A745]">
+        <li
+          className="
+inline-flex items-center p-2
+md:hover:bg-[#28A74540]
+md:hover:text-[#28A745]
+max-md:hover:border-b-2
+max-md:hover:border-[#28A745]
+max-md:hover:text-[#28A745]
+"
+        >
           <Mytours className="inline-block align-middle" />
           <Link className=" pr-3" href="/profile/mytours">
             تور های من
           </Link>
         </li>
-        <li className="inline-flex max-md:hover:underline p-2 hover:rounded-b-[9px]  hover:bg-[#28A74540] hover:text-[#28A745]">
+        <li
+          className="
+inline-flex items-center p-2
+md:hover:bg-[#28A74540]
+md:hover:text-[#28A745]
+md:hover:rounded-b-[9px]
+max-md:hover:text-[#28A745]
+
+
+"
+        >
           <Transaction />
           <Link className="pr-3" href="/profile/transactions">
             تراکنش ها

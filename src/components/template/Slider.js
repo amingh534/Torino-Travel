5b115b9546e5b32d8bfc0eb7fb0b508dc24dd570
Amin Ghashghai <amingh534@gmail.com>
2026-01-08 01:25:17 +0330

    slider added and developed

--- src/components/template/Slider.js
+++ src/components/template/Slider.js
@@ -1,42 +1,56 @@
+"use client";
 import styles from "./Slider.module.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+
+import { Pagination, Navigation } from "swiper/modules";
 
 function Slider() {
   return (
     <div className={styles.container}>
       <div className={styles.right}>
-        <div>
+        <div className={styles.title}>
           <span>؟</span>
           <h2>
-            چرا<i>؟</i>
+            چرا <i></i>
           </h2>
         </div>
 
-        <h3>تور طبیعت گردی و تاریخی </h3>
+        <h3>تور طبیعت‌گردی و تاریخی</h3>
         <p>
           اگر دوست داشته باشید که یک جاذبه طبیعی را از نزدیک ببینید و در دل
-          طبیعت چادر بزنید یا در یک اقامتگاه بوم گردی اتاق بگیرید، باید تورهای
-          طبیعت‌گردی را خریداری کنید. اما اگر بخواهید از جاذبه‌های گردشگری و
-          آثار تاریخی یک مقصد خاص بازدید کنید، می‌توانید تورهای فرهنگی و تاریخی
-          را خریداری کنید.
+          طبیعت چادر بزنید یا در یک اقامتگاه بوم‌گردی اتاق بگیرید، باید تورهای
+          طبیعت‌گردی را خریداری کنید...
         </p>
       </div>
+
       <div className={styles.left}>
         <div className={styles.imgcontainer}>
-          <div className={styles.slide1}>
-            <img src="images/slide1.webp" />
-          </div>
-          <div className={styles.slide2}>
-            <img src="images/slide2.webp" />
-          </div>
-          <div className={styles.slide3}>
-            <img src="images/slide3.webp" />
-          </div>
-          <div className={styles.slide4}>
-            <img src="images/slide4.webp" />
-          </div>
+          <Swiper
+            pagination={{ type: "fraction" }}
+            navigation={true}
+            modules={[Pagination, Navigation]}
+            className={styles.swiper}
+          >
+            
+            <SwiperSlide className={styles.swiperSlide}>
+              <img className={styles.slide1} src="images/slide1.webp" />
+            </SwiperSlide>
+            <SwiperSlide className={styles.swiperSlide}>
+              <img className={styles.slide2} src="images/slide2.webp" />
+            </SwiperSlide>
+            <SwiperSlide className={styles.swiperSlide}>
+              <img className={styles.slide3} src="images/slide3.webp" />
+            </SwiperSlide>
+            <SwiperSlide className={styles.swiperSlide}>
+              <img className={styles.slide4} src="images/slide4.webp" />
+            </SwiperSlide>
+          </Swiper>
         </div>
       </div>
-      
     </div>
   );
 }
